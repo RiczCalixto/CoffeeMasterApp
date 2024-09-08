733cd6ff6f3d8f1125f447354876d2eb0cef0c0e
Ricardo Calixto <38529592+RiczCalixto@users.noreply.github.com>
2024-09-08 11:52:58 -0300

    feat: first time with swiftUI / first commit

--- CoffeeMasterApp/ContentView.swift
+++ CoffeeMasterApp/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var name = ""
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Type your name!", text: $name)
+//            if name.count > 0 {
+//                Text("Hello from the other size \(name)")
+//            } else {
+//                /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
+//            }
+            name.count > 0 ? AnyView(Text("Hello from the other side \(name)")) : AnyView(EmptyView())
+            Button("label") {
+                print(name.count)
+            }
         }
-        .padding()
     }
 }
 
