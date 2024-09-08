c2c9d2be4dfd76924d3243fd12471108236590c4
Ricardo Calixto <38529592+RiczCalixto@users.noreply.github.com>
2024-09-07 22:02:18 -0300

    Initial Commit

---
+++ CoffeeMasterApp/CoffeeMasterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMasterAppApp.swift
+//  CoffeeMasterApp
+//
+//  Created by Taqtile on 07/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMasterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
